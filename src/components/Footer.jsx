00855741ebfbb184c81d8855978f01ac1a742cc8
Wemy Moyela <64646104+moyela@users.noreply.github.com>
2022-01-18 22:05:55 +0100

    update footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import '/.Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+
+            <div className='footer-left'>
+                <h3>
+                    <span className="bold-B">B</span>eBike
+                </h3>
+
+                <p>
+                    Trafalgar provides progressive and affordable{'\n'} healthcare accessible on mobile and online{'\n'} for everyone.
+                </p>
+
+                <p>
+                    &copy;eBike 2021. All rghts reserved.
+                </p>
+            </div>
+
+            <div className='footer-right'>
+
+
+            </div>
+
+        </div>
+    )
+}
